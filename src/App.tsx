65b4d2988f276dbe545ef04c887006a950869006
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-04-30 18:39:47 -0300

    feat: authentication page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import { Home } from "./pages/Home"
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Home />
   );  
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,33 @@
+import ilustrationImg from '../assets/images/illustration.svg'
+import logo from '../assets/images/logo.svg'
+import google from '../assets/images/google-icon.svg'
+
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+
+export function Home() {
+  return (
+    <div id="auth-page">
+      <aside>
+        <img src={ilustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao vivo.</strong>
+        <p>Tire duvidas de sua audiencia em tempo real.</p>
+      </aside>
+      <main>
+        <div className='main-content'>
+          <img src={logo} alt="letmeask"/>
+          <button className='create-room'>
+            <img src={google} alt="" />
+            Crie sua sala com o Google
+          </button>
+            <div className='separator'>Ou entre em uma sala </div>
+            <form>
+              <input type="text" placeholder="Digite o código da sala" />
+              <Button type="submit">Entrar na sala</Button>
+            </form>
+        </div>
+      </main>
+    </div>
+  )
+}
