ac78c3e6337ccb191f76aeb818abaf19a0daf3d5
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-05-01 12:17:08 -0300

    feat: NewRoom page added

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,30 @@
+import ilustrationImg from '../assets/images/illustration.svg'
+import logo from '../assets/images/logo.svg'
+import google from '../assets/images/google-icon.svg'
+
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+
+export function NewRoom() {
+  return (
+    <div id="auth-page">
+      <aside>
+        <img src={ilustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao vivo.</strong>
+        <p>Tire duvidas de sua audiencia em tempo real.</p>
+      </aside>
+      <main>
+        <div className='main-content'>
+          <img src={logo} alt="letmeask"/>
+            <h2>Crie uma nova sala</h2>
+            <form>
+              <input type="text" placeholder="Digite o código da sala" />
+              <Button type="submit">Criar sala</Button>
+            </form>
+            <p>Quer entrar em uma sala já existente? <a href="#">Clique aqui</a> </p> 
+        </div>
+      </main>
+    </div>
+  )
+}
